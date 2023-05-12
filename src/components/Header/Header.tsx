8bdd6ab28690f79c1c0820667c0cdc8c1cc9d25f
Vova Pol <ya-chai777@yandex.ru>
2023-05-12 19:12:44 +0300

    написал Header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Header.css';
+import { Link } from 'react-router-dom';
+
+export default function Header() {
+  return (
+    <header className="header">
+      <nav className="header__navigation">
+        <Link to="/" className="header__logo"></Link>
+        <Link to="/" className="header__link">
+          Главная
+        </Link>
+        <Link to="/" className="header__link">
+          Карточки
+        </Link>
+        <Link to="/" className="header__link">
+          Видео
+        </Link>
+        <Link to="/" className="header__link">
+          Поиск
+        </Link>
+        <Link to="/" className="header__link">
+          Добавить
+        </Link>
+      </nav>
+    </header>
+  );
+}
