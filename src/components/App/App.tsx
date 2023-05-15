896909a453f7bfbdc696d7291d31f9deb51ccf6d
Vova Pol <ya-chai777@yandex.ru>
2023-05-15 10:39:16 +0300

    добавил компонент Main

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
 import Header from '../Header/Header';
+import { Routes, Route } from 'react-router-dom';
+import Main from '../../pages/Main';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <main></main>
+      <main>
+        <Routes>
+          <Route path="/" element={<Main />} />
+        </Routes>
+      </main>
     </div>
   );
 }
---
+++ src/pages/Main.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Main.css';
+
+export default function Main() {
+  return (
+    <div className="main">
+      <h1 className="main__title">今天学了中文</h1>
+      <span className="main__subtitle">进了一步。</span>
+    </div>
+  );
+}
