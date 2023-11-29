f37f66944f5840da8c809d530b9fdc9789f4b37d
Vova Pol <ya-chai777@yandex.ru>
2023-11-29 14:16:11 +0300

    Удалены фыйлы redux

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import './index.css';
 import App from './components/App/App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { store } from './store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -12,9 +11,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <BrowserRouter basename="/my-chinese-tool">
     <React.StrictMode>
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <App />
     </React.StrictMode>
   </BrowserRouter>,
 );
