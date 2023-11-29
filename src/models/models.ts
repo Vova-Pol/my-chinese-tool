9f563aeddde2035f304ee1d09d3f544cd8fa737d
Vova Pol <ya-chai777@yandex.ru>
2023-11-29 14:16:30 +0300

    Правки в типах

--- src/models/models.ts
+++ src/models/models.ts
@@ -3,16 +3,28 @@ export interface IWord {
   pinyin: string;
   translation: string;
   usage?: string;
-  id: number;
+  _id: number;
 }
 
 export interface IChunk {
   wordsList: IWord[];
-  id: number;
   startTime: string;
+  _id: number;
 }
 
 export interface ISearchedWord {
   character: string;
   frequency: number;
+}
+
+export interface ISaveChunkValues {
+  wordsList: ISaveWordValues[];
+  startTime: string;
+}
+
+export interface ISaveWordValues {
+  character: string;
+  pinyin: string;
+  translation: string;
+  usage?: string;
 }
