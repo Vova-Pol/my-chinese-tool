fe3bf01440ee11a8597b78dc382f2ba2b0af21af
Vova Pol <ya-chai777@yandex.ru>
2023-12-06 14:58:57 +0300

    Правки по верстке в Main

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -7,8 +7,8 @@ import { LastAdded } from '../../components/LastAdded/LastAdded';
 export default function Main() {
   return (
     <div className="main">
-      <h1 className="main__title">今天学了中文</h1>
-      <span className="main__subtitle">进了一步。</span>
+      <h1 className="main__title">My Chinese Tool</h1>
+      <span className="main__subtitle">你好, 这是你的中文老师</span>
       <Profile />
       <Progress />
       {/* <LastAdded /> */}
